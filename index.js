26e07f1b035dea5c9b50e356333e716f2aa60fe5
Mriganka Saha <mrigankasaha2@gmail.com>
2021-06-03 08:46:32 +0530

    Add Cors

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const app = express();
 const PORT = 8080;
+const cors = require("cors");
+
+app.use(cors());
 app.use(express.json());
 
 app.listen(PORT, () => {
